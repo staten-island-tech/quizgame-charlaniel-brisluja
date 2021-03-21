cd8e4f2908ddfe9697bb3459dfccd586c2d858f0
Charley13 <charley.baluja22@sitechhs.com>
2021-03-21 17:59:19 -0400

    mmmm

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+DOMSelectors = {
+  wholeContainer: document.querySelector(".container"),
+  wholeAdventure: document.querySelector(".adventure"),
+  nextButton: document.querySelector(".next"),
+};
+//we might need these?? Idk
+const story = 0;
+const answer = "";
+
+const questionsAndAnswers = {
+  start: {
+    question: "",
+    answers: {},
+  },
+};
