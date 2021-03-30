4bc8eab2804aa030718877eb3434b17a86584466
Charley13 <charley.baluja22@sitechhs.com>
2021-03-29 21:28:22 -0400

    didn't do much but it's okay

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,21 @@
 import { DOMSelectors } from "./DOM";
 import { quizGame, endings } from "./questions";
 
-function hellYeah() {
-  const um = document.getElementById("next");
-  um.addEventListener("click", alert("trying"));
+//listen to selection of answer choice (a-d)
+//when next button is clicked, take answerIndexValue of answer choice
+//then within the quiz game array, find the object whose eventIndexValue === answerIndexValue of answer choice
+//grab object w/ correpsonding index value
+//display said object
+//rinse and repeat
+
+// function hellYeah() {
+//   const um = document.getElementById("next");
+//   next.addEventListener("click", alert("hiya"));
+// }
+document.getElementById("next").onclick = function () {
+  myFunction();
+};
+
+function myFunction() {
+  document.getElementById("next").innerHTML = "YOU CLICKED ME!";
 }
-hellYeah();
