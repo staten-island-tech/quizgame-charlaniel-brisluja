3826f2e77d119fdb77e379c166be5be04a40119d
Charley13 <charley.baluja22@sitechhs.com>
2021-03-29 20:35:30 -0400

    reverting

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import { DOMSelectors } from "./DOM";
+import { quizGame, endings } from "./questions";
+
+//accept input of answer choice, take answerIndexValue of input and find the object within quizGame whose eventIndexValue === answerIndexValue of input, display, go to step one for next question
+
+$("#next").onclick(function () {
+  alert("I'm not gonna curse");
+});
