cfa94b8228f90f07979666acf2674013c1e7030d
popplio2 <danikbrisk@yahoo.com>
2021-04-02 20:48:18 -0400

    couldnt figure stuff out :(

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import { DOMSelectors } from "./DOM";
-import { quizGame, endings } from "./questions";
+import { quizGame } from "./questions";
 
 //listen to selection of answer choice (a-d)
 //when next button is clicked, take answerIndexValue of answer choice
@@ -19,15 +19,47 @@ const nextButton = document.querySelector('#next');
 nextButton.addEventListener('click', function() {
   const selectedEvent = quizGame.find(event => event.eventIndex === currentAnswerIndex);
   //TEST: on click of onward button, change h1 to text of eventIndex = 0
+  const optionsArray = selectedEvent.options;
   const displayContainer = document.querySelector('.container');
   displayContainer.innerHTML =
   `<div id="quiz">
   <h1 id="text">${selectedEvent.question}</h1>
    <div id="button-options" class="button-box">
-      <button class="button button1">${selectedEvent.options[0].a}</button>
-      <button class="button button2">${selectedEvent.options[1].b}</button>
-      <button class="button button3">${selectedEvent.options[2].c}</button>
-      <button class="button button4">${selectedEvent.options[3].d}</button>
+      <button class="button button1">${optionsArray[0].a}</button>
+      <button class="button button2">${optionsArray[1].b}</button>
+      <button class="button button3">${optionsArray[2].c}</button>
+      <button class="button button4">${optionsArray[3].d}</button>
   </div> 
   <a class="next">Onwards!</a>`;
+
+  const buttonOne = document.getElementById('button1');
+  const buttonTwo = document.getElementById('button2');
+  const buttonThree = document.getElementById('button3');
+  const buttonFour = document.getElementById('button4');
+  
+  /*if (optionsArray.length = 1) {
+buttonOne.style.display = 'inline-block';
+buttonTwo.style.display = 'none';
+buttonThree.style.display = 'none';
+buttonFour.style.display = 'none';
+  } 
+  else if (optionsArray.length = 2) {
+    buttonOne.style.display = 'inline-block';
+    buttonTwo.style.display = 'inline-block';
+    buttonThree.style.display = 'none';
+    buttonFour.style.display = 'none';
+  } 
+  else if (optionsArray.length = 3) {
+    buttonOne.style.display = 'inline-block';
+    buttonTwo.style.display = 'inline-block';
+    buttonThree.style.display = 'inline-block';
+    buttonFour.style.display = 'none';
+  } 
+  else {
+    buttonOne.style.display = 'inline-block';
+buttonTwo.style.display = 'inline-block';
+buttonThree.style.display = 'inline-block';
+buttonFour.style.display = 'inline-block';
+  }
+  */
 });
