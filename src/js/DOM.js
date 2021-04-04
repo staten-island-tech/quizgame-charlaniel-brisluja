ca1dc69e518ceab708168bdb4e7f34534e96e7ee
Charley13 <charley.baluja22@sitechhs.com>
2021-04-04 15:12:30 -0400

    Revert "reverting pt 2"
    This reverts commit 7243c109c92b8c2e34f50ae68403bb1d21578f08.

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,13 +1,5 @@
 const DOMSelectors = {
-  displayContainer: document.getElementByClassName("container"),
-  buttonBox: document.getElementByClassName("button-box"),
-  buttonOne: document.getElementById("button1"),
-  buttonTwo: document.getElementById("button2"),
-  buttonThree: document.getElementById("button3"),
-  buttonFour: document.getElementById("button4"),
-  nextButton: document.getElementById("next"),
+  displayContainer: document.querySelector(".container"),
 };
 
 export { DOMSelectors };
-
-//figure out DOMSelectors
