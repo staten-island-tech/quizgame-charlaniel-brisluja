7243c109c92b8c2e34f50ae68403bb1d21578f08
Charley13 <charley.baluja22@sitechhs.com>
2021-04-04 15:12:20 -0400

    reverting pt 2

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,13 @@
 const DOMSelectors = {
-  displayContainer: document.querySelector(".container"),
+  displayContainer: document.getElementByClassName("container"),
+  buttonBox: document.getElementByClassName("button-box"),
+  buttonOne: document.getElementById("button1"),
+  buttonTwo: document.getElementById("button2"),
+  buttonThree: document.getElementById("button3"),
+  buttonFour: document.getElementById("button4"),
+  nextButton: document.getElementById("next"),
 };
 
 export { DOMSelectors };
+
+//figure out DOMSelectors
--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,33 @@
 import { DOMSelectors } from "./DOM";
 import { quizGame } from "./questions";
 
+const displayContainer = document.getElementByClassName("container");
+const buttonBox = document.getElementByClassName("button-box");
+const buttonOne = document.getElementById("button1");
+const buttonTwo = document.getElementById("button2");
+const buttonThree = document.getElementById("button3");
+const buttonFour = document.getElementById("button4");
+const nextButton = document.getElementById("next");
+
 let currentAnswerIndex = 1;
 
-const nextButton = document.querySelector('#next');
-//first, display starting event with eventIndex = 0 using innerHTML ✅ 
+// buttonBox.addEventListener("click", function () {
+//   alert("shit's happenin' man");
+//   console.log(buttonBox);
+// });
+
+//first, display starting event with eventIndex = 0 using innerHTML ✅
 //on click of option button, register currentAnswerIndex according to answerIndex of selected option (Do this next!!)
 //on click of next button, find event whose eventIndex matches the newly updated currentAnswerIndex ✅
 //use inner HTML to display that event, which closes the function and returns back to step 1!!! woohoo ✅
 
-nextButton.addEventListener('click', function() {
-  const selectedEvent = quizGame.find(event => event.eventIndex === currentAnswerIndex);
+nextButton.addEventListener("click", function () {
+  const selectedEvent = quizGame.find(
+    (event) => event.eventIndex === currentAnswerIndex
+  );
   //TEST: on click of onward button, change h1 to text of eventIndex = 0
 
-const buttonOne = document.querySelector('#button1');
-  const buttonTwo = document.querySelector('#button2');
-  const buttonThree = document.querySelector('#button3');
-  const buttonFour = document.querySelector('#button4');
-
-/* 
+  /* 
 if (selectedEvent.options[1].b === '' && selectedEvent.options[2].c === '' && selectedEvent.options[3].d === '') {
 buttonOne.style.visibility = 'visible';
 buttonTwo.style.visibility = 'hidden';
@@ -44,9 +53,7 @@ buttonThree.style.visibility = 'visible';
 buttonFour.style.visibility = 'visible';
   }*/
 
-  const displayContainer = document.querySelector('.container');
-  displayContainer.innerHTML =
-  `<div id="quiz">
+  displayContainer.innerHTML = `<div id="quiz">
   <h1 id="text">${selectedEvent.question}</h1>
    <div id="button-options" class="button-box">
       <button class="button button1">${selectedEvent.options[0].a}</button>
