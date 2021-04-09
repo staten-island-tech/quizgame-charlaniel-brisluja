bf4ab46c097b9d86a292a4945b37d42e080e6d3a
popplio2 <danikbrisk@yahoo.com>
2021-04-09 08:27:49 -0400

    Update questions.js

--- src/js/questions.js
+++ src/js/questions.js
@@ -160,7 +160,7 @@ const quizGame = [
     question:
       "You slide a small piece of metal into the keyhole, poking and prodding until you feel a click. The door swings open.",
     options: [
-      { a: "You're a real rougish fellow, aren't you?", answerIndex: 10 },
+      { a: "Walk through", answerIndex: 10 },
       { b: "" },
       { c: "" },
       { d: "" },
