ef3c031d256d4741a7d63dc0277b6e56b2a72a7e
popplio2 <danikbrisk@yahoo.com>
2021-04-09 11:10:27 -0400

    Revert "Update app.js"
    This reverts commit feb59ab4ab3dd082842bb520e827afd693b0e69d.

--- src/js/app.js
+++ src/js/app.js
@@ -2,55 +2,110 @@ import { DOMSelectors } from "./DOM";
 import { quizGame } from "./questions";
 
 function start() {
+  //let keyIndex = null; trying to figure out how to add key object to state, can't do it until rest of nonsense gets figured out
+
   //sets currentAnswerIndex to the event page we are currently on, -1 is the welcome page
   let currentAnswerIndex = -1;
+  let currentKeyState = undefined;
+  let currentPositiveValue = 0;
+  let currentNegativeValue = 0;
   //finds the event whose eventIndex equals the CAI thereby finding the welcome page
-  let selectedEvent = quizGame.find(
-    (event) => event.eventIndex === currentAnswerIndex
-  );
-  //inserts info from welcome page (CAI -1) into html
-  DOMSelectors.displayContainer.innerHTML = `<div id="quiz">
-  <h1 id="text">${selectedEvent.question}</h1>
-   <div id="button-options" class="button-box">
-      <button class="button button1">${selectedEvent.options[0].a}</button>
-      <button class="button button2">${selectedEvent.options[1].b}</button>
-      <button class="button button3">${selectedEvent.options[2].c}</button>
-      <button class="button button4">${selectedEvent.options[3].d}</button>
-  </div> 
-  <a class="next">Onwards!</a>`;
+
   //THESE DON'T WORK ARE MEAN TO US
   //intent is to set the CAI equal to the answerIndex that is updated on click of OPTION button and ONWARDS button (it should be just at option button rn)
   //addEventListener isn't working rn, the rest is
-  DOMSelectors.buttonOne.addEventListener("click", function () {
-    currentAnswerIndex = selectedEvent.options[0].answerIndex;
-    alert(currentAnswerIndex);
-  });
-  DOMSelectors.buttonTwo.addEventListener("click", function () {
-    currentAnswerIndex = selectedEvent.options[1].answerIndex;
-    alert(currentAnswerIndex);
-  });
-  DOMSelectors.buttonThree.addEventListener("click", function () {
-    currentAnswerIndex = selectedEvent.options[2].answerIndex;
-    alert(currentAnswerIndex);
-  });
-  DOMSelectors.buttonFour.addEventListener("click", function () {
-    currentAnswerIndex = selectedEvent.options[3].answerIndex;
-    alert(currentAnswerIndex);
-  });
-  //this is supposed to repeat what is done for the start of the game above with the subsequent eventIndexes and CAIs, it works but isn't currently b/c the above function is not working
-  DOMSelectors.nextButton.addEventListener("click", function () {
+
+  //inserts info from welcome page (CAI -1) into html, we are having issues w/ it b/c it breaks the going from button option 0 to next page
+  
+  //shit dont work
+  
+  //DOMSelectors.nextButton.onclick = displayEvent;
+  window.onload = displayEvent;
+
+  function displayEvent() {
     let selectedEvent = quizGame.find(
       (event) => event.eventIndex === currentAnswerIndex
     );
-    DOMSelectors.displayContainer.innerHTML = `<div id="quiz">
-    <h1 id="text">${selectedEvent.question}</h1>
-     <div id="button-options" class="button-box">
-        <button class="button button1">${selectedEvent.options[0].a}</button>
-        <button class="button button2">${selectedEvent.options[1].b}</button>
-        <button class="button button3">${selectedEvent.options[2].c}</button>
-        <button class="button button4">${selectedEvent.options[3].d}</button>
-    </div> 
-    <a class="next">Onwards!</a>`;
+    DOMSelectors.question.innerHTML = selectedEvent.question;
+    DOMSelectors.buttonOne.innerHTML = selectedEvent.options[0].a;
+    DOMSelectors.buttonTwo.innerHTML = selectedEvent.options[1].b;
+    DOMSelectors.buttonThree.innerHTML = selectedEvent.options[2].c;
+    DOMSelectors.buttonFour.innerHTML = selectedEvent.options[3].d;
+    if (
+      selectedEvent.options[1].b === "" &&
+      selectedEvent.options[2].c === "" &&
+      selectedEvent.options[3].d === ""
+    ) {
+      DOMSelectors.buttonOne.style.display = "inline-block";
+      DOMSelectors.buttonTwo.style.display = "none";
+      DOMSelectors.buttonThree.style.display = "none";
+      DOMSelectors.buttonFour.style.display = "none";
+    } else if (
+      selectedEvent.options[2].c === "" &&
+      selectedEvent.options[3].d === ""
+    ) {
+      DOMSelectors.buttonOne.style.display = "inline-block";
+      DOMSelectors.buttonTwo.style.display = "inline-block";
+      DOMSelectors.buttonThree.style.display = "none";
+      DOMSelectors.buttonFour.style.display = "none";
+    } else if (selectedEvent.options[3].d === "") {
+      DOMSelectors.buttonOne.style.display = "inline-block";
+      DOMSelectors.buttonTwo.style.display = "inline-block";
+      DOMSelectors.buttonThree.style.display = "inline-block";
+      DOMSelectors.buttonFour.style.display = "none";
+    } else {
+      DOMSelectors.buttonOne.style.display = "inline-block";
+      DOMSelectors.buttonTwo.style.display = "inline-block";
+      DOMSelectors.buttonThree.style.display = "inline-block";
+      DOMSelectors.buttonFour.style.display = "inline-block";
+    }
+    if (
+      selectedEvent.options[1].b === "" &&
+      selectedEvent.options[2].c === "" &&
+      selectedEvent.options[3].d === ""
+    ) {
+      DOMSelectors.buttonBox.style.gridTemplateColumns = "1fr";
+      DOMSelectors.buttonBox.style.padding = "0 25%";
+    } else {
+      DOMSelectors.buttonBox.style.gridTemplateColumns = "1fr 1fr";
+      DOMSelectors.buttonBox.style.padding = "0";
+    }
+    if (selectedEvent.eventIndex === 5) {
+      currentKeyState = true;
+      alert("You found a key! This will come in handy later...");
+    } else if (selectedEvent.eventIndex === 6) {
+      alert("You're a real roguish fellow, aren't ye?");
+    } else if (selectedEvent.eventIndex === 16.5) {
+      alert("Ha! Choose a real option, wise guy! (see what I did there? hehe)");
+      }
+    if (selectedEvent.hasOwnProperty("negativeValue")) {
+      currentNegativeValue += selectedEvent.negativeValue;
+      alert(currentNegativeValue);
+    }
+    if (selectedEvent.hasOwnProperty("positiveValue")) {
+      currentPositiveValue += selectedEvent.positiveValue;
+      alert(currentPositiveValue);
+    }
+  }
+
+  document.addEventListener("click", function (option) {
+    let selectedEvent = quizGame.find(
+      (event) => event.eventIndex === currentAnswerIndex
+    );
+    if (option.target === DOMSelectors.buttonOne) {
+      currentAnswerIndex = selectedEvent.options[0].answerIndex;
+      //alert(currentAnswerIndex);
+    } else if (option.target === DOMSelectors.buttonTwo) {
+      currentAnswerIndex = selectedEvent.options[1].answerIndex;
+      //alert(currentAnswerIndex);
+    } else if (option.target === DOMSelectors.buttonThree) {
+      currentAnswerIndex = selectedEvent.options[2].answerIndex;
+      //alert(currentAnswerIndex);
+    } else if (option.target === DOMSelectors.buttonFour) {
+      currentAnswerIndex = selectedEvent.options[3].answerIndex;
+      //alert(currentAnswerIndex);
+    }
+    displayEvent();
   });
 }
 
